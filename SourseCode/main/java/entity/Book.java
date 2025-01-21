5f11685b7e23838129204072a38e95e123524622
Bampalikis <132650984+Bampalikis@users.noreply.github.com>
2025-01-21 19:59:42 +0200

    Create Book.java

---
+++ SourseCode/main/java/entity/Book.java
@@ -0,0 +1,133 @@
+package org.libapp.libapp.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "books", schema = "bookapp", uniqueConstraints = {
+        @UniqueConstraint(name = "isbn", columnNames = {"isbn"})
+})
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "book_id", nullable = false)
+    private Integer id;
+
+    @Column(name = "copies_available")
+    private Integer copiesAvailable;
+
+    @Lob
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Lob
+    @Column(name = "isbn", nullable = false)
+    private String isbn;
+
+    @Column(name = "publication_date")
+    private LocalDate publicationDate;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "publisher_id", nullable = false, referencedColumnName = "publisher_id", foreignKey = @ForeignKey(name = "fk_publisher_book"))
+    private Publisher publisher;
+
+    @Lob
+    @Column(name = "description")
+    private String description;
+
+    @Lob
+    @Column(name = "cover_image_url")
+    private String coverImageUrl;
+
+    @Column(name = "author_id")
+    private Integer authorId;
+
+    @Lob
+    @Column(name = "genre")
+    private String genre;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public LocalDate getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(LocalDate publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCoverImageUrl() {
+        return coverImageUrl;
+    }
+
+    public void setCoverImageUrl(String coverImageUrl) {
+        this.coverImageUrl = coverImageUrl;
+    }
+
+    public Integer getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(Integer authorId) {
+        this.authorId = authorId;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public Publisher getPublisherId() {
+        return publisher;
+    }
+
+    public Integer getCopiesAvailable() {
+        return copiesAvailable;
+    }
+
+    public void setCopiesAvailable(Integer copiesAvailable) {
+        this.copiesAvailable = copiesAvailable;
+    }
+}
